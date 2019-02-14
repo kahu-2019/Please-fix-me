c6a41696f09e80ec7ce4f512bd056aa3e5647de3
Rebecca Jackson <zimbecs@gmail.com>
2019-02-15 12:21:22 +1300

    added my link to app and made it stateful

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,14 +1,32 @@
 import React from 'react'
+import {Link} from 'react-dom'
 
-// This might need to be turned into a stateful (class-based) component
-const App = () => (
-  <div className='app'>
-    <h1>Please Fix Me - I is broked</h1>
-    <div className='karl'></div>
-    <div className='miju'></div>
-    <div className='becs'></div>
-    <div className='richard'></div>
-  </div>
-)
+import Becs from './Becs'
+
+class App extends React.Component {
+  
+  render(){
+    return(
+      <Fragment>
+        <div className='app'>
+          <h1>Please Fix Me - I is broked</h1>
+        <div className='karl'></div>
+
+        <div className='miju'>
+        
+        </div>
+
+        <div className='becs'>
+          <button>
+            <Link to='/becs' component={Becs} />
+          </button>
+        </div>
+
+        <div className='richard'></div>
+        </div>
+      </Fragment>
+    )
+  }
+}
 
 export default App
