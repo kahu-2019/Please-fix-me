93619dfcf2cd138f77c1a97ce632d21c4feca0f2
Richard Graham <richgrahamdev@gmail.com>
2019-02-15 11:58:44 +1300

    added css grid

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 // This might need to be turned into a stateful (class-based) component
 const App = () => (
   <div className='app'>
-    Ready to rock and roll
+    <h1>Please Fix Me - I is broked</h1>
+    <div className='karl'></div>
+    <div className='miju'></div>
+    <div className='becs'></div>
+    <div className='richard'></div>
   </div>
 )
 
